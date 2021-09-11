6bb83aa278d46e5dc171eaa927b7e9727a8b84c6
link-mozy <rnjs213@gmail.com>
2021-09-11 19:52:23 +0900

    add (bearer) ResourceServer 추가

--- oauth2/src/main/java/com/rest/oauth2/config/Oauth2AuthorizationConfig.java
+++ oauth2/src/main/java/com/rest/oauth2/config/Oauth2AuthorizationConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
@@ -53,10 +54,17 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
     /**
      * 토큰 정보를 DB를 통해 관리한다.
      */
-//    @Override
-//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-//        endpoints.tokenStore(new JdbcTokenStore(dataSource));
-//    }
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(new JdbcTokenStore(dataSource));
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()") // allow check token
+                .allowFormAuthenticationForClients();
+    }
 
     /**
      * 토큰 발급 방식을 JWT 토큰 방식으로 변경한다. 이렇게 하면 토큰 저장하는 DB Table은 필요가 없다.
@@ -64,21 +72,21 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
      * @param endpoints
      * @throws Exception
      */
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        super.configure(endpoints);
-        endpoints.accessTokenConverter(jwtAccessTokenConverter()).userDetailsService(customUserDetailService);
-    }
+//    @Override
+//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//        super.configure(endpoints);
+//        endpoints.accessTokenConverter(jwtAccessTokenConverter()).userDetailsService(customUserDetailService);
+//    }
 
     /**
      * jwt converter를 등록
      *
      * @return
      */
-    @Bean
-    public JwtAccessTokenConverter jwtAccessTokenConverter() {
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey(signKey);
-        return converter;
-    }
+//    @Bean
+//    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+//        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+//        converter.setSigningKey(signKey);
+//        return converter;
+//    }
 }
